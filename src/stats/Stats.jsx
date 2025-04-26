1d05d046dbb2371fd1ff29ea97e1106e143b6944
W-Rouse <rousewr@gmail.com>
2025-04-26 00:59:03 -0400

    - Added Dropdown to filter DB search

--- src/stats/Stats.jsx
+++ src/stats/Stats.jsx
@@ -1,5 +1,10 @@
 import React, { useState, useEffect } from "react";
 import SearchBar from "../Components/Common/SearchBar";
+import { Box } from "@mui/material";
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
 
 
 const dividerStyle = {
@@ -20,9 +25,33 @@ const dividerBoxStyle = {
     width: '100%',
 }
 
+
 const Stats = () => {
+    const [statCategory, setStatCategory] = useState('');
+
+    const handleStatCategoryChange = (event) => {
+        setStatCategory(event.target.value);
+    };
     return (
-        <div><SearchBar/></div>
+        <Box sx={dividerBoxStyle}>
+            <div>
+                <FormControl variant="standard" sx={{ m: 1, minWidth: 120 }}>
+                    <InputLabel id="category-input-label">Category</InputLabel>
+                    <Select
+                        id="stats-category-select"
+                        value={statCategory}
+                        onChange={handleStatCategoryChange}
+                        label="Stat Category"
+                        defaultValue=""
+                    >
+                        <MenuItem value={"Team"}>Team</MenuItem>
+                        <MenuItem value={"Player"}>Player</MenuItem>
+                        <MenuItem value={"Matchup"}>Matchup</MenuItem>
+                    </Select>
+                </FormControl>
+                <SearchBar />
+            </div>
+        </Box>
     );
 };
 
